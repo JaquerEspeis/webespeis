53e46401e0c6eaf5faa2a53713ef4cc02a3350f0
Jeffrey Esquivel S. <jeff@estudiomanati.com>
2015-08-18 09:03:16 -0600

    Updated SITE_URL to point to new domain

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "JáquerEspeis"  # (translatable)
 BLOG_TITLE = "JáquerEspeis"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://jaquerespeis.github.io"
+SITE_URL = "http://www.jaquerespeis.org"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
